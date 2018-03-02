d0b7a5a682cb37e8d21b84c3e48167468c44ad58
kiluakun <kiluakun@gmail.com>
2018-03-02 09:27:09 -0300

    Carga inicial de archivos

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Goku' },
+  { id: 2, name: 'Luffy' },
+  { id: 3, name: 'Naruto' },
+  { id: 4, name: 'Kirito' },
+  { id: 5, name: 'Champion of Ash' },
+  { id: 6, name: 'Senjougahara' },
+  { id: 7, name: 'Wolverine' },
+  { id: 8, name: 'Dr Strange' },
+  { id: 9, name: 'Thor' },
+  { id: 10, name: 'Entei' }
+];
